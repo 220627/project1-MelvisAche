b80b4a37cfbcce7ac75bf159c6afd481d60aa9e7
MelvisAche <91048180+MelvisAche@users.noreply.github.com>
2022-07-25 02:27:09 -0400

    Added Front end

---
+++ MyHTML/MyP1/Login.js
@@ -0,0 +1,20 @@
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var myVar;
+function myFunction(){
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage(){
+document.getElementById("loader").style.display = "none";
+document.getElementById("myDiv").style.display = "block";
+
+}
